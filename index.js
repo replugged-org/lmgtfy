3d71b27e39675c603fcb323d58711e336e38b63f
kittenvr <103120356+kittenvr@users.noreply.github.com>
2022-08-16 23:15:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ module.exports = class LMGTFY extends Plugin {
 
     return {
       send: true,
-      result: `<https://googlethatforyou.com?q=${queryString}>`
+      result: `<https://googlethatforyou.com?${queryString}>`
     };
   }
 
